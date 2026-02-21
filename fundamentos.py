6ca90df1a6cb06b2fc1723cc83f2a40a3ecd2668
soyrick <ricardoenriquegr27@gmail.com>
2026-02-21 12:10:31 -0400

    first commit

---
+++ fundamentos.py
@@ -0,0 +1,11 @@
+print (" Hola Mundo")
+#Variables
+numero1 = 10
+numero2 = 20 
+resultado = numero1 + numero2   
+print(f"La suma es: {resultado}")
+print("La suma es: " + str(resultado))
+print("La suma es: {}".format(resultado))
+
+potencia = numero1 ** numero2
+print(f"{numero1} elevado a {numero2} es: {potencia}")
